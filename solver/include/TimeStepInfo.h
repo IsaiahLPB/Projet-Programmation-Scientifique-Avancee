95c622e093565f2a3cde49d3ced433bc180b16c0
emre.onal <emre.g.onal@gmail.com>
2025-04-16 15:03:42 +0200

    Improved computation time

---
+++ solver/include/TimeStepInfo.h
@@ -0,0 +1,9 @@
+#ifndef TIMESTEPINFO_H
+#define TIMESTEPINFO_H
+
+struct TimeStepInfo {
+    double t;
+    int stepcounter;
+};
+
+#endif
--- solver/src/main.py
+++ solver/src/main.py
@@ -1,16 +1,36 @@
 import solver
 import numpy as np
 import math
-import sys
-
-# Constants
-nx, ny = 101, 101
-x_min, x_max, y_min, y_max = -10, 10, -10, 10
-dt_vals = [0.02/800, 0.02/40, 0.02/4]
-t_max = 10
-which_method = 0 # 0-FTCS, 1-BTCS, 2-CTCS
+import json
+from solver import Solver, TimeStepInfo
 
 # Read JSON file
+with open("../consts.JSON", "r") as f:
+    data = json.load(f)
+
+# Get constants 
+constants = data["constantes"]
+param = data["paramètres utilisateurs"]
+
+nx,ny = constants["n_x"], constants["n_y"]
+x_min, x_max, y_min, y_max = constants["x_min"], constants["x_max"], constants["y_min"], constants["y_max"]
+method = param["method"]
+t_max = param["t_max"]
+dt = param["dt"]
+if dt == "default":
+	match method:
+		case "FTCS":
+			dt = 0.02/800
+			method_id = 0
+		case "BTCS":
+			dt = 0.02/40
+			method_id = 1
+		case "CTCS":
+			dt = 0.02/4
+			method_id = 2
+		case default:
+			print("Error : This method is not implemented")
+			exit(1)
 
 # Definition of V (will be retrived in the DB)
 x_vec = np.linspace(x_min, x_max, nx)
@@ -38,35 +58,27 @@ for i in range (0, nx):
 		psi_imag[i, j] = gauss * math.sin(theta)
 
 
-if(len(sys.argv) != 2):
-	print("Error: usage <int>")
-methode_id = int(sys.argv[1])
-assert(methode_id <= 2)
-assert(methode_id >= 0)
-
 V = np.asfortranarray(V)
 psi_real = np.asfortranarray(psi_real)
 psi_imag = np.asfortranarray(psi_imag)
 
-solver = solver.Solver(methode_id, V)
-dt = dt_vals[methode_id]
-t = 0
-stepcounter = 0
-match methode_id:
-	case 0:
-		while t < t_max:
-			solver.FTCS_derivation(psi_real, psi_imag)
-			if stepcounter % 10000 == 0:
+solver = solver.Solver(V)
+info = TimeStepInfo()
+info.t = 0.0
+info.stepcounter = 0
+
+match method:
+	case "FTCS":
+		while info.t < t_max:
+			solver.FTCS_derivation(psi_real, psi_imag, info)
 			# Write the files in the DB 
-				re_filename = f"data/FTCS_psi_{stepcounter}_re.csv"
-				im_filename = f"data/FTCS_psi_{stepcounter}_im.csv"
-				np.savetxt(re_filename, psi_real, delimiter=',')
-				np.savetxt(im_filename, psi_imag, delimiter=',')
-				print("File", stepcounter, "written")
-			stepcounter +=1 
-			t += dt
+			re_filename = f"data/FTCS_psi_{info.stepcounter}_re.csv"
+			im_filename = f"data/FTCS_psi_{info.stepcounter}_im.csv"
+			np.savetxt(re_filename, psi_real, delimiter=',')
+			np.savetxt(im_filename, psi_imag, delimiter=',')
+			print("File", info.stepcounter, "written")
 		print("FTCS completed")
-	case 1:
+	case "BTCS":
 		while t < t_max:
 			solver.BTCS_derivation(psi_real, psi_imag)
 			if stepcounter % 500 == 0:
@@ -75,10 +87,11 @@ match methode_id:
 				im_filename = f"data/BTCS_psi_{stepcounter}_im.csv"
 				np.savetxt(re_filename, psi_real, delimiter=',')
 				np.savetxt(im_filename, psi_imag, delimiter=',')
+				print("File", stepcounter, "written")
 			stepcounter +=1 
 			t += dt
 		print("BTCS completed")
-	case 2:
+	case "CTCS":
 		while t < t_max:
 			solver.CTCS_derivation(psi_real, psi_imag)
 			if stepcounter % 50 == 0:
@@ -87,9 +100,13 @@ match methode_id:
 				im_filename = f"data/CTCS_psi_{stepcounter}_im.csv"
 				np.savetxt(re_filename, psi_real, delimiter=',')
 				np.savetxt(im_filename, psi_imag, delimiter=',')
+				print("File", stepcounter, "written")
 			stepcounter +=1 
 			t += dt
 		print("CTCS completed")
+	case default:
+		print("This method is not implemented")
+		exit(1)
 
 
 
