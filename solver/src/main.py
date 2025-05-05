6e374107b7bbdb3d00f8046dd2e5635a0afc4798
emre.onal <emre.g.onal@gmail.com>
2025-05-05 15:03:21 +0200

    Creating .vti files

--- solver/src/main.py
+++ solver/src/main.py
@@ -40,7 +40,7 @@ psi_imag = np.imag(psi)
 psi_abs2 = np.abs(psi)**2
 
 # TEMPORARY 
-#db.CreateExperience(exp_name, "../consts.JSON", V)
+db.CreateExperience(exp_name, "../consts.JSON", V)
 
 # Retrive data from de DB
 #V = db.GetPotential(exp_name)
